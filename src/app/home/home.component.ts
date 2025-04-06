5b6b582b6c8c7e8abd3c4e23a448911361e5f4e2
Kim Madula <Kim@gmail.com>
2025-04-06 23:04:23 +0800

    Implement profile management, admin dashboard, and fake backend

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { AccountService } from '@app/_services';
+import { AccountService } from '../_services';
 
 @Component({ templateUrl: 'home.component.html' })
 export class HomeComponent {
